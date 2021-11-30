27df51c49d8aac6d8153303d81563d4cfa008dc1
ghern <ghern@DESKTOP-OCUVVH0>
2021-11-30 10:40:29 -0600

    Somewhat done

---
+++ Cereal/Cereal/Cereal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cereal
+{
+    public class Cereal
+    {
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Calories { get; set; }
+        public double Cups { get; set; }
+        public Cereal()
+        {
+            Manufacturer = string.Empty;
+            Name = string.Empty;
+            Calories = 0;
+            Cups = 0;
+        }
+        public Cereal(string manufacturer, string name, double calories, double cups)
+        {
+            Manufacturer = manufacturer;
+            Name = name;
+            Calories = calories;
+            Cups = cups;
+        }
+        public override string ToString()
+        {
+            return $"{Name} is made by {Manufacturer} and has {Calories.ToString()} calories for every {Cups.ToString()} cup of serving";
+        }
+    }
+}
---
+++ Cereal/Cereal/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Cereal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                string[] linesofData = File.ReadAllLines("Cereal_Data.txt");
+                List<Cereal> CerealName = new List<Cereal>();
+                string Cname = "";
+                string Mname = "";
+                double calories = 0;
+                double cup = 0;
+                string calNote = "";
+
+                for (int i = 1; i < linesofData.Length; i++)
+                {
+                    string line = linesofData[i];
+                    string[] partofLine = line.Split("|");
+                    Cname = partofLine[0];
+                    Mname = partofLine[1];
+                    calories = Convert.ToDouble(partofLine[2]);
+                    cup = Convert.ToDouble(partofLine[3]);
+                    if (cup >= 1)
+                    {
+                        Cereal C = new Cereal();
+                        C.Manufacturer = Mname;
+                        C.Name = Cname;
+                        C.Calories = calories;
+                        C.Cups = cup;
+                        CerealName.Add(C);
+                    }
+                }
+                for (int i = 0; i < CerealName.Count; i++)
+                {
+                    if (calories <= 100)
+                    {
+                        calNote = ".This cereal has less than 100 calories.";
+                    }
+                    else
+                    {
+                        calNote = "";
+                    }
+                }
+                Console.WriteLine("These cereals have a serving size of one or more.");
+                foreach (var cereal in CerealName)
+                {
+                    Console.WriteLine(cereal + "" + calNote);
+                }
+
+            }
+        }
+    }
+}
